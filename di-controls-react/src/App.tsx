2dde8c8287b3974e41832845ebb3ea1ee8d4e182
S. Isaac Kellogg <isaac.kellogg@quantellia.com>
2025-02-20 11:16:03 -0500

    Display example CDD with draggable elements
    App.tsx now reads the example JSON and generates draggable blue div boxes with the element name and causal type as inner content.
    Also draws dependency arrows using Xarrow.

--- di-controls-react/src/App.tsx
+++ di-controls-react/src/App.tsx
@@ -1,38 +1,57 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import graphData from "./schema_compliant_cdd.json" assert {type: "json"};
-import { Gauge, StackedBarplot } from "./components/DataVisualizations";
-import { CheckBoxGroup, Slider } from "./components/BasicControls";
-import * as d3 from "d3";
 import Draggable from "react-draggable";
 import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
-import { Dial } from "./components/Controls";
 
 function App() {
   
   const updateXarrow = useXarrow();
   
+  //Generate HTML for dependency arrows
+  const dependencyArrows = graphData.diagrams[0].dependencies.map((dep) => (
+    <Xarrow
+      key={dep.meta.uuid}
+      start={dep.source}
+      end={dep.target}
+      strokeWidth={2}
+      curveness={0.4}
+    />
+  ))
+
+  //Generate HTML for diagram elements
+  const diagramElements = graphData.diagrams[0].elements.map((elem) => {
+    return (
+      <Draggable
+        defaultPosition={elem.content.position}
+        onDrag={updateXarrow}
+        onStop={updateXarrow}
+        key={"draggable-" + elem.meta.uuid}
+      >
+        <div 
+          id={elem.meta.uuid}
+          style={{
+            border: "2px solid #000000",
+            backgroundColor: "#4f5af8",
+            width: "200px",
+            color: "#ffffff",
+            padding: "5px",
+            position: "absolute"
+          }}
+        >
+          {elem.meta.name}
+          <hr style={{border: "1px solid black", marginInline: "-5px"}}/>
+          {elem.causalType}
+        </div>
+      </Draggable>
+    )
+  })
 
   return (
     <Xwrapper>
-      <div style={{ fontFamily: "sans-serif" }}>
-        <div>
-        {graphData.diagrams[0].elements.map((elem) => {
-            return (
-              <Draggable
-                defaultPosition={elem.content.position}
-                onDrag={updateXarrow}
-                onStop={() => {
-                  updateXarrow();
-                }}
-                key={"draggable-" + elem.meta.uuid}
-              >
-                <div>
-                  <legend>{elem.meta.name}</legend>
-                </div>
-              </Draggable>
-            )
-          })}
-        </div>
+      <div style={{ fontFamily: "sans-serif"}}>
+        {/* Draw arrows BELOW element boxes */}
+        {dependencyArrows}
+        {diagramElements}
       </div>
     </Xwrapper>
   );
